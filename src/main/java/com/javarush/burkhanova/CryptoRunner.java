5b5e0aa65db7083fab1417e135e07fd07bd7d4dc
EBurkhanova <eburkhanova95@gmail.com>
2025-02-25 18:48:46 +0100

    FIX Author

--- src/main/java/com/javarush/burkhanova/CryptoRunner.java
+++ src/main/java/com/javarush/burkhanova/CryptoRunner.java
@@ -1,5 +1,8 @@
 package com.javarush.burkhanova;
 
+/**
+ *  Author E. Burkhanova
+ */
 public class CryptoRunner {
     public static void main(String[] args) {
         System.out.println("I did it");

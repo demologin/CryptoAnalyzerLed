9d8132c7dcd487bfb76ee629fe9ef25d49f1d02d
akhmelev <akhmelev@gmail.com>
2025-02-25 20:45:23 +0300

    ADD CryptoRunner

--- src/main/java/com/javarush/petrov/CryptoRunner.java
+++ src/main/java/com/javarush/petrov/CryptoRunner.java
@@ -2,6 +2,6 @@ package com.javarush.petrov;
 
 public class CryptoRunner {
     public static void main(String[] args) {
-        System.out.println("I do it!");dsdsd
+        System.out.println("I do it!");
     }
 }

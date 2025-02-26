c47f2f334f66cb786a2c251100e56f6b1dc49411
ichesnokov <deroman23@gmail.com>
2025-02-26 17:55:25 +0700

    MODIFY ConsoleRunner and Menu

--- src/main/java/com/javarush/chesnokov/ConsoleRunner.java
+++ src/main/java/com/javarush/chesnokov/ConsoleRunner.java
@@ -4,20 +4,14 @@ import java.util.Scanner;
 
 public class ConsoleRunner {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Menu menu = new Menu();
-        menu.showMenu();
-        int command = menu.chooseCommand(scanner);
+        Scanner scanner = new Scanner(System.in);
 
-        String result = switch (command) {
-            case Menu.COMMAND_ENCRYPT -> "Вы выбрали Encrypt";
-            case Menu.COMMAND_DECRYPT -> "Вы выбрали Decrypt";
-            case Menu.COMMAND_BRUTE_FORCE -> "Вы выбрали Brute force";
-            case Menu.COMMAND_ANALYZE -> "Вы выбрали Analyze";
-            case Menu.COMMAND_EXIT -> "Выход из программы";
-            default -> "Неизвестная команда";
-        };
-        System.out.println(result);
+        while (true) {
+            menu.showMenu();
+            int command = menu.chooseCommand(scanner);
+            menu.executeCommand(command);
+        }
 
     }
 }
--- src/main/java/com/javarush/chesnokov/Menu.java
+++ src/main/java/com/javarush/chesnokov/Menu.java
@@ -1,5 +1,7 @@
 package com.javarush.chesnokov;
 
+import com.javarush.chesnokov.command.*;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -12,37 +14,44 @@ public class Menu {
     public static final int COMMAND_ANALYZE = 4;
     public static final int COMMAND_EXIT = 5;
 
-    private final Map<Integer, String> commands = new HashMap<>();
+    private final Map<Integer, Command> commands = new HashMap<>();
 
     public Menu() {
         initCommands();
     }
 
     private void initCommands() {
-        commands.put(COMMAND_ENCRYPT, "Encrypt");
-        commands.put(COMMAND_DECRYPT, "Decrypt");
-        commands.put(COMMAND_BRUTE_FORCE, "Brute force");
-        commands.put(COMMAND_ANALYZE, "Analyze");
-        commands.put(COMMAND_EXIT, "Exit");
+        commands.put(COMMAND_ENCRYPT, new Encrypt());
+        commands.put(COMMAND_DECRYPT, new Decrypt());
+        commands.put(COMMAND_BRUTE_FORCE, new BruteForce());
+        commands.put(COMMAND_ANALYZE, new Analyze());
+        commands.put(COMMAND_EXIT, new Exit());
     }
 
     public void showMenu() {
         System.out.println("-".repeat(20));
         System.out.println("Выберите команду:");
-        for (var map : commands.entrySet()) {
-            System.out.println(map);
+        for (var entry : commands.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue().getClass().getSimpleName());
         }
         System.out.println("-".repeat(20));
     }
 
-    public int chooseCommand(Scanner scanner) {
+    public void executeCommand(int command) {
+        Command cmd = commands.get(command);
+        if (cmd != null) {
+            cmd.execute();
+        } else {
+            System.out.println("Неверная команда!");
+        }
+    }
 
+    public int chooseCommand(Scanner scanner) {
         while (!scanner.hasNextInt()) {
             System.out.println("Ошибка! Введите целое число.\n");
             scanner.next();
             showMenu();
         }
-
         return scanner.nextInt();
     }
 

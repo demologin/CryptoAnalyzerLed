aeb27bcf15f350a4a6385f734b305f2e24921054
UserRome <test@.gmail.com>
2025-02-28 00:10:47 +0300

    CREATE class Menu

---
+++ src/main/java/com/javarush/user_roman/Menu.java
@@ -0,0 +1,8 @@
+package com.javarush.user_roman;
+
+public class Menu {
+    public static void startMenu() {
+        System.err.println("Выберите действие:");
+        System.out.print("\t1-Запись текста в файл \n \t2-Чтение текста из файла \n \t3-Шифрование \n \t4-Расшифровка \n \t5-Выход \n");
+    }
+}

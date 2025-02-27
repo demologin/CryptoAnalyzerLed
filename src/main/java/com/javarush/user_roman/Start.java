7dbbd139d60d07a6bfdd61aae210ade6e8a8c079
UserRome <test@.gmail.com>
2025-02-27 21:56:00 +0300

    REMOVE Hello World

--- src/main/java/com/javarush/user_roman/Start.java
+++ src/main/java/com/javarush/user_roman/Start.java
@@ -3,6 +3,5 @@ package com.javarush.user_roman;
 public class Start {
     public static void main(String[] args) {
         System.out.println("I do it!");
-        System.out.println("Hello World!");
     }
 }

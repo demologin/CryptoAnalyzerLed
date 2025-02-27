e9271b2da117d77e37f1a4e86e9b7526e54735bf
UserRome <test@.gmail.com>
2025-02-27 23:46:50 +0300

    ADD Switch ADD WriteTextToFile ADD ReadTextFromFile

--- src/main/java/com/javarush/user_roman/Start.java
+++ src/main/java/com/javarush/user_roman/Start.java
@@ -1,7 +1,33 @@
 package com.javarush.user_roman;
 
+import java.util.Scanner;
+
 public class Start {
     public static void main(String[] args) {
-        System.out.println("I do it!");
+        System.err.println("Выберите действие:");
+        System.out.print("\t1-Запись текста в файл \n \t2-Чтение текста из файла \n \t3-Шифрование \n \t4-Расшифровка \n \t5-Выход \n");
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        switch (button) {
+            case 1:
+                WriteTextToFile.writeTextToFile();
+                break;
+            case 2:
+                System.out.println("Чтение текста из файла");
+                ReadTextFromFile.readTextFromFile();
+                break;
+            case 3:
+                System.out.println("Шифрование");
+                break;
+            case 4:
+                System.out.println("Расшифровка");
+                break;
+            case 5:
+                System.out.println("Выход");
+                break;
+            default:
+                System.out.println("Нет такой команды");
+        }
     }
+
 }

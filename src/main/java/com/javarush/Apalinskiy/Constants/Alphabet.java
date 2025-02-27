53afd7ddbdba442491ff095ba4251b8e2ed2f373
Mikhail Apalinski <apalinskiy.mihkail@gmail.com>
2025-02-27 15:26:45 +0400

    ADD Alphabet

---
+++ src/main/java/com/javarush/Apalinskiy/Constants/Alphabet.java
@@ -0,0 +1,22 @@
+package com.javarush.Apalinskiy.Constants;
+
+import java.util.ArrayList;
+
+public class Alphabet {
+    private static final String ALPHABET = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    private static final String SYMBOLS = ",.? \";:'";
+
+    public ArrayList <Character> AlphabetToList(){
+        ArrayList<Character> alphabet = new ArrayList<>();
+        char[] chars = ALPHABET.toCharArray();
+        for (char aChar : chars) {
+            alphabet.add(aChar);
+        }
+        return alphabet;
+    }
+
+    public String getSYMBOLS() {
+        return SYMBOLS;
+    }
+}
+

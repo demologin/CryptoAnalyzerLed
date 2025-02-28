077f4c59607da1960b2aaa880ccc4ae0cbe22fa0
UserRome <test@.gmail.com>
2025-02-28 10:22:33 +0300

    ADD while ADD try catch

--- src/main/java/com/javarush/user_roman/Start.java
+++ src/main/java/com/javarush/user_roman/Start.java
@@ -1,5 +1,6 @@
 package com.javarush.user_roman;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Start {
@@ -8,35 +9,34 @@ public class Start {
         Menu.startMenu();
 
         Scanner sc = new Scanner(System.in);
-        int button = sc.nextInt();
-        switch (button) {
-            case 1:
-                WriteTextToFile.writeTextToFile();
-                break;
-            case 2:
-                System.out.println("Чтение текста из файла");
-                ReadTextFromFile.readTextFromFile();
-                break;
-            case 3:
-                System.out.println("Шифрование");
-                break;
-            case 4:
-                System.out.println("Расшифровка");
-                break;
-            case 5:
-                System.out.println("Выход");
-                break;
-            default:
-                System.out.println("Нет такой команды");
-        }
-        /*while (true) {
-            String print = sc.nextLine();
-            System.out.println(print);
-            if (print.equals("exit")) {
-                System.out.println("Выход");
-                break;
+        Boolean isExit = false;
+        while (!isExit) {
+            try {
+                int button = sc.nextInt();
+                switch (button) {
+                    case 1:
+                        WriteTextToFile.writeTextToFile();
+                        break;
+                    case 2:
+                        System.out.println("Чтение текста из файла");
+                        ReadTextFromFile.readTextFromFile();
+                        break;
+                    case 3:
+                        System.out.println("Шифрование");
+                        break;
+                    case 4:
+                        System.out.println("Расшифровка");
+                        break;
+                    case 5:
+                        System.out.println("Выход");
+                        isExit = true;
+                        break;
+                    default:
+                        System.out.println("Нет такой команды");
+                }
+            } catch (InputMismatchException e) {
+                throw new InputMismatchException("Вы ввели не число. Все сломалось");
             }
-        }*/
+        }
     }
-
 }

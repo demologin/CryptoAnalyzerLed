b865cc7a79d8a320168ca75b9ca4a016543ff480
UserRome <test@.gmail.com>
2025-02-28 11:34:54 +0300

    REFACTOR switch from Start to Menu

--- src/main/java/com/javarush/user_roman/Menu.java
+++ src/main/java/com/javarush/user_roman/Menu.java
@@ -1,5 +1,8 @@
 package com.javarush.user_roman;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Menu {
     public static void startMenu() {
         System.err.println("Выберите действие:");
@@ -8,5 +11,38 @@ public class Menu {
                 + "\t3-Шифрование \n "
                 + "\t4-Расшифровка \n "
                 + "\t5-Выход \n");
+    }
+
+    static void switchMenu() {
+        Scanner sc = new Scanner(System.in);
+        Boolean isExit = false;
+        while (!isExit) {
+            try {
+                int button = sc.nextInt();
+                switch (button) {
+                    case 1:
+                        WriteTextToFile.writeTextToFile();
+                        break;
+                    case 2:
+                        System.out.println("Чтение текста из файла");
+                        ReadTextFromFile.readTextFromFile();
+                        break;
+                    case 3:
+                        System.out.println("Шифрование");
+                        break;
+                    case 4:
+                        System.out.println("Расшифровка");
+                        break;
+                    case 5:
+                        System.out.println("Выход");
+                        isExit = true;
+                        break;
+                    default:
+                        System.out.println("Нет такой команды");
+                }
+            } catch (InputMismatchException e) {
+                throw new InputMismatchException("Вы ввели не число. Все сломалось");
+            }
+        }
     }
 }
--- src/main/java/com/javarush/user_roman/Start.java
+++ src/main/java/com/javarush/user_roman/Start.java
@@ -1,42 +1,9 @@
 package com.javarush.user_roman;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Start {
 
     public static void main(String[] args) {
         Menu.startMenu();
-
-        Scanner sc = new Scanner(System.in);
-        Boolean isExit = false;
-        while (!isExit) {
-            try {
-                int button = sc.nextInt();
-                switch (button) {
-                    case 1:
-                        WriteTextToFile.writeTextToFile();
-                        break;
-                    case 2:
-                        System.out.println("Чтение текста из файла");
-                        ReadTextFromFile.readTextFromFile();
-                        break;
-                    case 3:
-                        System.out.println("Шифрование");
-                        break;
-                    case 4:
-                        System.out.println("Расшифровка");
-                        break;
-                    case 5:
-                        System.out.println("Выход");
-                        isExit = true;
-                        break;
-                    default:
-                        System.out.println("Нет такой команды");
-                }
-            } catch (InputMismatchException e) {
-                throw new InputMismatchException("Вы ввели не число. Все сломалось");
-            }
-        }
+        Menu.switchMenu();
     }
 }

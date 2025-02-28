9129d6714241df7c38f8f5b79b4fb2c492a644b9
pyatigin.a.a <pyatigin.a.a@gmail.com>
2025-02-28 15:20:11 +0500

    Get Option Fix

---
+++ src/main/java/com/javarush/pyatigin/constatnt/PopularLetters.java
@@ -0,0 +1,23 @@
+package com.javarush.pyatigin.constatnt;
+
+public class PopularLetters {
+    private static final char[] popularLetters={' ','е','а','и','н','т','с','р','л'};
+
+    public static int getPopularLettersLength() {
+        return popularLetters.length;
+    }
+    public static char getCharPopularLetters(int index) {
+        return popularLetters[index];
+    }
+
+    //Подумать как вернуть Большие буквы
+    public static int getIndexOfpopularLetters(char c) {
+        for (int i = 0; i < popularLetters.length; i++) {
+            if (popularLetters[i] == Character.toLowerCase(c)) {
+                return i;
+            }
+        }
+        //Подумать как через ошибку вернуть что нет char
+        return -1;
+    }
+}

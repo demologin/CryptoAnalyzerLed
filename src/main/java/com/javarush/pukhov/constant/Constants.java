329afcec13ebd24fc2abfba710e428ed6aec03d0
Johnrac <johnracer@mail.ru>
2025-02-28 14:04:13 +0300

    ADD constants

---
+++ src/main/java/com/javarush/pukhov/constant/Constants.java
@@ -0,0 +1,9 @@
+package com.javarush.pukhov.constant;
+
+public class Constants {
+    public static final String ENCRYPT = "Encrypt";
+    public static final String DECRYPT = "Decrypt";
+    public static final String BRUTE_FORCE = "Brute force";
+    public static final String ANALYZE = "Analyze";
+    public static final String EXIT = "Exit";
+}

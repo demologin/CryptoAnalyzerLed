9ac26a8f48f62b0670f55aefbb07047dc1b8640b
Johnrac <johnracer@mail.ru>
2025-03-01 20:11:17 +0300

    ADD toString for actions

--- src/main/java/com/javarush/pukhov/command/ActionName.java
+++ src/main/java/com/javarush/pukhov/command/ActionName.java
@@ -1,5 +1,26 @@
 package com.javarush.pukhov.command;
 
+import com.javarush.pukhov.constant.Constants;
+
 public enum ActionName {
-    ENCRYPT, DECRYPT_KEY, BRUTEFORCE, ANALYZE, EXIT
+    ENCRYPT(Constants.ENCRYPT),
+    DECRYPT_KEY(Constants.DECRYPT),
+    BRUTEFORCE(Constants.BRUTE_FORCE),
+    ANALYZE(Constants.ANALYZE),
+    EXIT(Constants.EXIT);
+
+    /**
+     * @param actionName
+     */
+    private ActionName(String actionName) {
+        this.actionName = actionName;
+    }
+
+    private String actionName;
+
+    @Override
+    public String toString() {
+        return actionName;
+    }
+
 }

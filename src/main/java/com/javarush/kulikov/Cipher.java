11e8bce0784da0fca63823aaf6d0fe3872a6d1eb
JediAnakin <elspiderman2307@gmail.com>
2025-03-02 18:50:49 +0400

    Added decryption methods, changed everything in the style of functional programming

--- src/main/java/com/javarush/kulikov/Cipher.java
+++ src/main/java/com/javarush/kulikov/Cipher.java
@@ -3,22 +3,43 @@ package com.javarush.kulikov;
 public class Cipher {
 
     public String encrypt(String text, int shift) {
-        StringBuilder encrypted = new StringBuilder();
         if (shift < 0) {
             throw new IllegalArgumentException("Сдвиг не может быть отрицательным");
         }
 
-        for (var character : text.toCharArray()) {
-            if (Character.isLetter(character)) {
-                char base = Character.isLowerCase(character) ? 'а' : 'А';
-                character = (char) ((character - base + shift) % 33 + base);
-            }
-            encrypted.append(character);
-        }
-        return encrypted.toString();
+        return text.chars()
+                .mapToObj(ch -> encryptChar((char) ch, shift))
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
     }
 
+    private char encryptChar(char character, int shift) {
+        if (!Character.isLetter(character)) {
+            return character;
+        }
+
+        char base = Character.isLowerCase(character) ? 'а' : 'А';
+        return (char) ((character - base + shift) % 33 + base);
+    }
+
+
     public String decrypt(String encryptedText, int shift) {
-        return null;
+        if (shift < 0) {
+            throw new IllegalArgumentException("Сдвиг не может быть отрицательным");
+        }
+
+        return encryptedText.chars()
+                .mapToObj(ch -> decryptChar((char) ch, shift))
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
+    }
+
+    private char decryptChar(char character, int shift) {
+        if (!Character.isLetter(character)) {
+            return character;
+        }
+
+        char base = Character.isLowerCase(character) ? 'а' : 'А';
+        return (char) ((character - base - shift + 33) % 33 + base);
     }
 }
--- src/main/java/com/javarush/kulikov/MainApp.java
+++ src/main/java/com/javarush/kulikov/MainApp.java
@@ -3,6 +3,8 @@ package com.javarush.kulikov;
 public class MainApp {
     public static void main(String[] args) {
         Cipher cipher = new Cipher();
-        System.out.println(cipher.encrypt("Привет мир!", 0));
+        String string = cipher.encrypt("Привет мир!", 3);
+        System.out.println(string);
+        System.out.println(cipher.decrypt(string, 3));
     }
 }

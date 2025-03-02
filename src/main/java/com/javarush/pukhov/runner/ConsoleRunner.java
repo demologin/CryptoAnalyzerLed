d848f688216b178a2cde9c0071f8ea9e33580bca
Johnrac <johnracer@mail.ru>
2025-03-02 22:19:19 +0300

    FIX console runner

--- src/main/java/com/javarush/pukhov/runner/ConsoleRunner.java
+++ src/main/java/com/javarush/pukhov/runner/ConsoleRunner.java
@@ -1,13 +1,21 @@
 package com.javarush.pukhov.runner;
 
+import com.javarush.pukhov.io.ConsoleInput;
+import com.javarush.pukhov.io.ConsolePrinter;
+import com.javarush.pukhov.io.Input;
+import com.javarush.pukhov.io.Output;
+import com.javarush.pukhov.io.OutputError;
+import com.javarush.pukhov.view.console.ConsoleApp;
 
 public class ConsoleRunner implements Runner {
 
     @Override
     public void run() {
-        /*
-         * 
-         */
+        Input input = new ConsoleInput();
+        OutputError outputError = new ConsolePrinter();
+        Output output = outputError;
+        ConsoleApp app = new ConsoleApp(input, output, outputError);
+        app.start();
     }
 
 }

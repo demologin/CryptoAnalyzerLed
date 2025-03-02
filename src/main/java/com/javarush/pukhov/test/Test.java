d00529e054e2e0146436bc431774544fcd2c144d
Johnrac <johnracer@mail.ru>
2025-03-02 12:47:05 +0300

    ADD test testActionNameSize

--- src/main/java/com/javarush/pukhov/test/Test.java
+++ src/main/java/com/javarush/pukhov/test/Test.java
@@ -4,11 +4,18 @@ import com.javarush.pukhov.command.ActionName;
 
 public class Test {
     public static void main(String[] args) {
-        testActionName();
+        // testActionName("123");
+        testActionNameSize(10);
     }
 
-    private static void testActionName() {
-        ActionName name = ActionName.valueOf("123");
+    private static void testActionNameSize(int index) {
+        ActionName name = ActionName.values()[index];
         System.out.println(name);
     }
+
+    private static void testActionName(String checkString) {
+        ActionName name = ActionName.valueOf(checkString);
+        System.out.println(name);
+    }
+
 }

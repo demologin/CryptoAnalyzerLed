083a035e312f972fdb096ca3857fc3f6f955e0bb
Johnrac <johnracer@mail.ru>
2025-03-02 21:47:29 +0300

    ADD testStackTrace

--- src/main/java/com/javarush/pukhov/test/Test.java
+++ src/main/java/com/javarush/pukhov/test/Test.java
@@ -1,5 +1,7 @@
 package com.javarush.pukhov.test;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.StringJoiner;
 
 import com.javarush.pukhov.command.ActionName;
@@ -7,9 +9,29 @@ import com.javarush.pukhov.view.console.constants.Messages;
 
 public class Test {
     public static void main(String[] args) {
-        // testActionName("123");
+        testActionName("123");
         // testActionNameSize(10);
-        testErrorMessage();
+        // testErrorMessage();
+        // testStackTrace();
+    }
+
+    private static void testStackTrace() {
+        StringBuilder errorBuilder = new StringBuilder();
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        try {
+            throw new Exception("Check stack trace");
+        } catch (Exception e) {
+            e.printStackTrace(pw);
+            // errorBuilder.append("Сообщение: ").append(e.getMessage()).append("\nСтек вызовов:\n").append(sw.toString());
+        }
+        try {
+            throw new Exception("New check stack trace");
+        } catch (Exception e) {
+            e.printStackTrace(pw);
+            errorBuilder.append("Сообщение: ").append(e.getMessage()).append("\nСтек вызовов:\n").append(sw.toString());
+        }
+        System.out.print(errorBuilder);
     }
 
     private static void testErrorMessage() {
@@ -17,11 +39,13 @@ public class Test {
         // joiner.add(Messages.INCORRECT_NUMBER);
         System.out.println(joiner.length());
         // joiner.add("%1$s - is not an action");
-        String message = String.format(joiner.toString(),"uipo");
+        String message = String.format(joiner.toString(), "uipo");
         System.out.println(message);
     }
 
     private static void testActionNameSize(int index) {
+        int size = ActionName.values().length;
+        System.out.println(size);
         ActionName name = ActionName.values()[index];
         System.out.println(name);
     }

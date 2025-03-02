dc9578c7afa17b92fae1fdef26efe9272a37e791
Johnrac <johnracer@mail.ru>
2025-03-02 13:25:49 +0300

    ADD error message test

--- src/main/java/com/javarush/pukhov/test/Test.java
+++ src/main/java/com/javarush/pukhov/test/Test.java
@@ -1,11 +1,24 @@
 package com.javarush.pukhov.test;
 
+import java.util.StringJoiner;
+
 import com.javarush.pukhov.command.ActionName;
+import com.javarush.pukhov.view.console.constants.Messages;
 
 public class Test {
     public static void main(String[] args) {
         // testActionName("123");
-        testActionNameSize(10);
+        // testActionNameSize(10);
+        testErrorMessage();
+    }
+
+    private static void testErrorMessage() {
+        StringJoiner joiner = new StringJoiner(" and ");
+        // joiner.add(Messages.INCORRECT_NUMBER);
+        System.out.println(joiner.length());
+        // joiner.add("%1$s - is not an action");
+        String message = String.format(joiner.toString(),"uipo");
+        System.out.println(message);
     }
 
     private static void testActionNameSize(int index) {

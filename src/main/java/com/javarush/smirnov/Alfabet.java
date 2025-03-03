607d9f7b8331617924d67c6c5e1471ac2b1db18d
LyohaSmi <smileha@mail.ru>
2025-03-03 17:55:41 +0300

    ADD Map Alfabet

--- src/main/java/com/javarush/smirnov/Alfabet.java
+++ src/main/java/com/javarush/smirnov/Alfabet.java
@@ -1,8 +1,20 @@
 package com.javarush.smirnov;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Alfabet {
-    private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
+    private static final char[] alfabet = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
             'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
             'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
-    public static final int LENGTH = ALPHABET.length;
+
+    public static final int length = alfabet.length;
+
+    public final static Map<Character, Integer> map = new HashMap<>();
+
+    static {
+        for (int i = 0; i < alfabet.length; i++) {
+            map.put(alfabet[i], i);
+        }
+    }
 }

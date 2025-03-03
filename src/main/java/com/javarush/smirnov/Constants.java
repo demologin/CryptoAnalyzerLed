9921ce292093915b24b45abd4690f3c61435fb74
LyohaSmi <smileha@mail.ru>
2025-03-03 11:46:15 +0300

    ADD new constants

--- src/main/java/com/javarush/smirnov/Constants.java
+++ src/main/java/com/javarush/smirnov/Constants.java
@@ -1,5 +1,8 @@
 package com.javarush.smirnov;
 
 public class Constants {
-
+    public static String[] start = {"program started", "choose an option:", "1. Encode", "2. Decode", "3. Exit"};
+    public static String incorInput = "incorrect input";
+    public static String end = "program ended";
+    public static String ok = "ok";
 }

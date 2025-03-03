8e3bd60af7a3851a450b99d642b392e61a7317b5
LyohaSmi <smileha@mail.ru>
2025-03-03 13:54:50 +0300

    ADD ALFABET.LENGTH

--- src/main/java/com/javarush/smirnov/Alfabet.java
+++ src/main/java/com/javarush/smirnov/Alfabet.java
@@ -4,5 +4,5 @@ public class Alfabet {
     private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
             'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
             'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
-
+    public static final int LENGTH = ALPHABET.length;
 }

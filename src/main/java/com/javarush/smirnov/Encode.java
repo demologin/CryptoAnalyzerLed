901dd39bef79c8d5b0ebf6c49d119ffdbd3ecb55
LyohaSmi <smileha@mail.ru>
2025-03-03 12:06:43 +0300

    ADD key

--- src/main/java/com/javarush/smirnov/Encode.java
+++ src/main/java/com/javarush/smirnov/Encode.java
@@ -1,4 +1,12 @@
 package com.javarush.smirnov;
 
 public class Encode {
+    int key;
+    public Encode(int key){
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
 }

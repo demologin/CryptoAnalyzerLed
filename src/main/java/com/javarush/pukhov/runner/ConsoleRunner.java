9ea419ac621c78aa6fa900ddcd55473c5f3d8ea7
Johnrac <johnracer@mail.ru>
2025-03-03 13:58:57 +0300

    DELETE output error

--- src/main/java/com/javarush/pukhov/runner/ConsoleRunner.java
+++ src/main/java/com/javarush/pukhov/runner/ConsoleRunner.java
@@ -4,7 +4,6 @@ import com.javarush.pukhov.io.ConsoleInput;
 import com.javarush.pukhov.io.ConsolePrinter;
 import com.javarush.pukhov.io.Input;
 import com.javarush.pukhov.io.Output;
-import com.javarush.pukhov.io.OutputError;
 import com.javarush.pukhov.view.console.ConsoleApp;
 
 public class ConsoleRunner implements Runner {
@@ -12,9 +11,8 @@ public class ConsoleRunner implements Runner {
     @Override
     public void run() {
         Input input = new ConsoleInput();
-        OutputError outputError = new ConsolePrinter();
-        Output output = outputError;
-        ConsoleApp app = new ConsoleApp(input, output, outputError);
+        Output output = new ConsolePrinter();
+        ConsoleApp app = new ConsoleApp(input, output);
         app.start();
     }
 
--- src/main/java/com/javarush/pukhov/view/console/ConsoleApp.java
+++ src/main/java/com/javarush/pukhov/view/console/ConsoleApp.java
@@ -7,28 +7,24 @@ import com.javarush.pukhov.command.Action;
 import com.javarush.pukhov.command.Exit;
 import com.javarush.pukhov.io.Input;
 import com.javarush.pukhov.io.Output;
-import com.javarush.pukhov.io.OutputError;
 import com.javarush.pukhov.view.console.menu.Menu;
 
 public class ConsoleApp {
     Input input;
     Output output;
-    OutputError outputError;
 
     /**
      * @param input
      * @param output
      */
-    public ConsoleApp(Input input, Output output, OutputError outputError) {
+    public ConsoleApp(Input input, Output output) {
         this.input = input;
         this.output = output;
-        this.outputError = outputError;
     }
 
     public void start() {
         Action action = null;
         do {
-            try {
                 Menu menu = Menu.getInstance();
                 output.print(menu.show());
                 action = menu.selectAction(input.read());
@@ -41,9 +37,6 @@ public class ConsoleApp {
                     }
                 }
                 action.execute(parameters);
-            } catch (RuntimeException e) {
-                outputError.print(e);
-            }
         } while (!(action instanceof Exit));
     }
 

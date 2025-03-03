b95338e56a85536cf1f386a502351a20522c2cdb
UserRome <test@.gmail.com>
2025-03-04 00:29:01 +0300

    MODIFE static variable MENU_ACTION

--- src/main/java/com/javarush/user_roman/FinalConstant.java
+++ src/main/java/com/javarush/user_roman/FinalConstant.java
@@ -4,13 +4,15 @@ import java.nio.file.Path;
 
 public class FinalConstant {
     private static final Path TEST_PATH = Path.of("src/main/java/com/javarush/user_roman/text/test.txt");
-    private static final Path TEXT_PATH = Path.of("src/main/java/com/javarush/user_roman/text/test.txt");
-    private static final String MENU_ACTION = "\t1-Запись текста в файл \n "
+    private static final Path TEXT_PATH = Path.of("src/main/java/com/javarush/user_roman/text/text.txt");
+    private static final String MENU_ACTION = "************************************************* \n"
+            + "\t1-Запись текста в файл \n "
             + "\t2-Чтение текста из записанного файла test.txt \n "
             + "\t3-Чтение текста из готового файла text.txt \n "
             + "\t4-Шифрование \n "
             + "\t5-Расшифровка \n "
-            + "\t6-Выход \n";
+            + "\t6-Выход \n"
+            + "************************************************* \n";
 
     public static Path getTest() {
         return TEST_PATH;

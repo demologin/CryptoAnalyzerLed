c0b88cd7582a1503d9c71099a480863677ede8f7
mikhailov <dm7723934@gmail.com>
2025-03-03 23:30:02 +0300

    FIX Fork Url

---
+++ src/main/java/com/javarush/mikhailov/MyProjected.java
@@ -0,0 +1,11 @@
+package com.javarush.mikhailov;
+
+import java.io.IOException;
+
+public class MyProjected {
+    public static void main(String[] args) throws IOException {
+        MyProject myProject = new MyProject();
+        MyFile file = new MyFile();
+
+    }
+}

40d87bd6505b675ea711604c0c7ab111d42ee6c4
MBabkin2026 <mak.babkin.2025@mail.ru>
2025-03-04 18:50:58 +0300

    ADD CharALFABET

--- src/main/java/com/javarush/babkin/Cipher.java
+++ src/main/java/com/javarush/babkin/Cipher.java
@@ -1,4 +1,10 @@
 package com.javarush.babkin;
 
 public class Cipher {
+
+    public static final char[] ALFABET = ('Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ',
+            'Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э',
+            'Я','Ч','С','М','И','Т','Ь','Б','Ю');
+
+
 }

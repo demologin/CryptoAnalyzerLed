b0765bb343f15537dd892a6e16e433e357ad6dee
Natalka8 <aleks.romik@gmail.com>
2025-03-05 21:00:16 +0300

    ADD CryptoRunner Created ConsoleRunner  class added a Scanner added characters to the alphabet

--- src/main/java/com/javarush/Larina/CryptoRunner.java
+++
@@ -1,7 +0,0 @@
-package com.javarush.Larina;
-
-public class CryptoRunner {
-     public static void main(String[] args){
-         System.out.println("I did it!");
-     }
-}
--- src/main/java/larina/ConsoleRunner.java
+++ src/main/java/larina/ConsoleRunner.java
@@ -1,19 +1,17 @@
 package larina;
-
+import java.text.*;
 import java.io.*;
 import java.util.Scanner;
 
 public class ConsoleRunner {
-    public static final String rusletters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-     public static void main(String[] args){
-         Scanner scanner = new Scanner(System.in);
-         System.out.println("\"C:\\hitech-plugin\\CryptoAnalyzerLed\\text\\text.txt\"");
-         String inputFilePath = scanner.nextLine();
-System.out.println("");
+    public static final String rusletters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
+            " ., ': -!?"; //Russian alphabet and punctuation
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст:");
+        String inputFilePath = scanner.nextLine();
+        System.out.println("Введите путь:");
+        String outputFilePath = scanner.nextLine();
+        int key = 8; //Encryption key
+        String text = readTextFromFile(inputFilePath);
 
-     }
-
-
-
-
-}

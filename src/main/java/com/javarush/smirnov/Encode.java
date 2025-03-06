f95f72ff195b1119b9676547284ba5727309e493
LyohaSmi <smileha@mail.ru>
2025-03-06 13:52:52 +0300

    FIX Encode

--- src/main/java/com/javarush/smirnov/Encode.java
+++ src/main/java/com/javarush/smirnov/Encode.java
@@ -1,12 +1,43 @@
 package com.javarush.smirnov;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Encode {
+
     int key;
+
     public Encode(int key){
         this.key = key;
     }
 
-    public int getKey() {
-        return key;
+    public void EnCode(){
+
+        Path pathFile = PathBuilder.GetPathFile();
+        Path pathSaveFileToEncoding = PathBuilder.GetPathFileToSaveEncoding();
+
+        int value;
+        int index;
+        try (BufferedReader bufRead = Files.newBufferedReader(pathFile);
+             BufferedWriter bufWrite = Files.newBufferedWriter(pathSaveFileToEncoding)){
+            while ((value = bufRead.read()) > -1){
+                char ch = (char) value;
+                ch = Character.toLowerCase(ch);
+                if (Alfabet.map.containsKey(ch) || ch == '\n') {
+                    if (ch == '\n'){
+                        bufWrite.write('\n');
+                    } else {
+                        index = (Alfabet.map.get(ch) + key) % Alfabet.length;
+                        bufWrite.write(Alfabet.alfabet[index]);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(Constants.incorInput);
+        }
+
     }
 }

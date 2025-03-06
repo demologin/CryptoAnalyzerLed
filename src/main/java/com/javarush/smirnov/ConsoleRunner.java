2d6b85d3097240d4ada4db1740c1890cfe837206
LyohaSmi <smileha@mail.ru>
2025-03-06 13:55:28 +0300

    ADD Encode and Decode in ConsoleRunner

--- src/main/java/com/javarush/smirnov/ConsoleRunner.java
+++ src/main/java/com/javarush/smirnov/ConsoleRunner.java
@@ -1,23 +1,32 @@
 package com.javarush.smirnov;
 
-import org.apache.commons.math3.analysis.function.Constant;
 
 import java.util.Scanner;
 
 public class ConsoleRunner {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         for (int i = 0; i < Constants.start.length; i++) {
             System.out.println(Constants.start[i]);
         }
-        Scanner scanner = new Scanner(System.in);
+
+        int mode = scanner.nextInt();
+
         try {
-            int mode = scanner.nextInt();
             switch (mode){
                 case 1:
-                    System.out.println(Constants.ok);
+                    System.out.println(Constants.key);
+                    int key = scanner.nextInt();
+                    Encode code = new Encode(key);
+                    code.EnCode();
                     break;
                 case 2:
-                    System.out.println(Constants.ok);
+                    System.out.println(Constants.key);
+                    int key2 = scanner.nextInt();
+                    Decode code2 = new Decode(key2);
+                    code2.DeCode();
                     break;
                 case 3:
                     System.out.println(Constants.end);

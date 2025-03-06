e118c2b79e86f32d765fa4ee4ccb64c965ee677b
Johnrac <johnracer@mail.ru>
2025-03-06 18:15:01 +0300

    ADD encapsulation

--- src/main/java/com/javarush/pukhov/constant/AlphabetCaesar.java
+++ src/main/java/com/javarush/pukhov/constant/AlphabetCaesar.java
@@ -11,15 +11,24 @@ public class AlphabetCaesar {
     private static final String RUS_SYMBOLS = "йцукенгшщзхъфывапролджэячсмитьбюё";
     private static final String PUNCTUATIONS = "\n”’:-!;*.,/\\?\"";
 
-    public static final char[] chars = (RUS_SYMBOLS + RUS_SYMBOLS.toUpperCase() + PUNCTUATIONS).toCharArray();
+    private static final char[] chars = (RUS_SYMBOLS + RUS_SYMBOLS.toUpperCase() + PUNCTUATIONS).toCharArray();
 
-    public static final Map<Character, Integer> alphabet = new HashMap<>();
+    private static final Map<Character, Integer> alphabet = new HashMap<>();
 
 
     static {
         for (int i = 0; i < chars.length; i++) {
             alphabet.put(chars[i], i);
         }
+    }
+
+    public static Map<Character, Integer> getAlphabet() {
+        Object clone = ((HashMap<Character, Integer>) alphabet).clone();
+        return (Map<Character, Integer>) clone;
+    }
+
+    public static char getSymbol(int index) {
+        return chars[index];
     }
 
 }

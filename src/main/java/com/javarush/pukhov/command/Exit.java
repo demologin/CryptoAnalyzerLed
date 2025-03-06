4dfb1c5b26ba787391d0273501e5bdeb250f558a
Johnrac <johnracer@mail.ru>
2025-03-06 18:31:37 +0300

    ADD action exit

--- src/main/java/com/javarush/pukhov/command/Exit.java
+++ src/main/java/com/javarush/pukhov/command/Exit.java
@@ -1,12 +1,19 @@
 package com.javarush.pukhov.command;
 
+import java.io.OutputStream;
+import java.io.Writer;
 import java.util.List;
+
+import com.javarush.pukhov.constant.Constants;
+import com.javarush.pukhov.io.ConsolePrinter;
+import com.javarush.pukhov.io.Output;
 
 public final class Exit implements Action {
 
+    Output<OutputStream, Writer> output = new ConsolePrinter();
+
     @Override
     public void execute(List<String> parameters) {
-
+        output.print(Constants.EXIT_PROGRAM);
     }
-
 }

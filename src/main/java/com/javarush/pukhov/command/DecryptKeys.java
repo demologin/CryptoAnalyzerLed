b99808f31f4e13015fbba30bc4007ee3e84024ef
Johnrac <johnracer@mail.ru>
2025-03-07 00:53:22 +0300

    ADD decryption with key

--- src/main/java/com/javarush/pukhov/command/DecryptKeys.java
+++ src/main/java/com/javarush/pukhov/command/DecryptKeys.java
@@ -1,21 +1,47 @@
 package com.javarush.pukhov.command;
 
-import java.nio.file.Path;
 import java.util.List;
 
-public final class DecryptKeys extends Decrypt<Path> {
+import com.javarush.pukhov.constant.Constants;
+import com.javarush.pukhov.io.FileInput;
+import com.javarush.pukhov.io.FileOutput;
+import com.javarush.pukhov.valid.ValidatorActionCaesar;
+import com.javarush.pukhov.valid.ValidatorCipherAction;
+
+public final class DecryptKeys extends Decrypt {
+    private int key;
+    FileInput fileInput;
+    FileOutput fileOutput;
 
     @Override
     public void decrypt(List<String> parameters) {
-        /*
-         * TODO: Add impl
-         */
+        validator = new ValidatorActionCaesar<>(this);
+        if (validator.check(parameters)) {
+            getValuesFrom(validator);
+
+            fileInput = new FileInput(src);
+            fileOutput = new FileOutput(destination);
+
+            processFileStream(fileInput, fileOutput, Option.RW);
+        }
+    }
+
+     @Override
+    protected void getValuesFrom(ValidatorCipherAction<List<String>> validator) {
+        super.getValuesFrom(validator);
+        key = ((ValidatorActionCaesar<List<String>>) validator).getKey();
+    }
+
+
+     @Override
+    public String toString() {
+        return Constants.DECRYPT;
     }
 
     @Override
-    protected void processSymbols(char[] buf, int countRead) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'processSymbols'");
+    protected char[] processSymbols(char[] buf, int countRead) {
+        ProcessorCipherCaesar cipherCaesar = new ProcessorCipherCaesar(key, DECRYPT_MODE);
+        return cipherCaesar.processSymbolsWithKey(buf, countRead);
     }
 
 }

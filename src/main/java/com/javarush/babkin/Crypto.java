371fc62109a31dbff28ebb2142bf861f10b0960c
MBabkin2026 <mak.babkin.2025@mail.ru>
2025-03-06 20:05:36 +0300

    ADD static fields in Crypto
    add static fields incrypt, dicrypt, getKeyToValue

--- src/main/java/com/javarush/babkin/Crypto.java
+++ src/main/java/com/javarush/babkin/Crypto.java
@@ -1,4 +1,58 @@
 package com.javarush.babkin;
 
+import java.util.Map;
+import java.util.Set;
+
 public class Crypto {
+    public static char[] incrypt(char[] input, int keyKrypt) {
+
+        int key = keyKrypt;
+        String scrText = "";
+
+        for (char symb : input) {
+            if (Dictionary.DICTIONARY.containsKey(symb)) {
+
+                int value = (Dictionary.DICTIONARY.get(symb) + key) % Dictionary.getLengthCHAR_SYMBOLS();
+                char scryptText = getKeyToValue(Dictionary.DICTIONARY, value);
+                scrText = scrText + scryptText;
+            } else {
+                scrText = scrText + symb;
+            }
+        }
+        char[] outcCharArray = scrText.toCharArray();
+        return outcCharArray;
+
+    }
+
+    public static char[] dicrypt(char[] input, int keyKrypt) {
+
+        int key = keyKrypt;
+        String scrText = "";
+
+        for (char symb : input) {
+            if (Dictionary.DICTIONARY.containsKey(symb)) {
+
+                int value = (Dictionary.DICTIONARY.get(symb) - key) % Dictionary.getLengthCHAR_SYMBOLS();
+                char scryptText = getKeyToValue(Dictionary.DICTIONARY, value);
+                scrText = scrText + scryptText;
+            } else {
+                scrText = scrText + symb;
+            }
+        }
+        char[] outcCharArray = scrText.toCharArray();
+        return outcCharArray;
+
+    }
+
+
+
+    public static Character getKeyToValue (Map map, Integer value){
+        Set<Map.Entry<Character, Integer>> entries = map.entrySet();
+        for(Map.Entry<Character, Integer> pair : entries) {
+            if (pair.getValue().equals(value)) {
+                return pair.getKey();
+            }
+        }
+        return null;
+    }
 }

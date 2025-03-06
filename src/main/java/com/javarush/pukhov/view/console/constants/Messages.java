6573971f0d78a5e85fedf0cdd9c16d44ffb93aeb
Johnrac <johnracer@mail.ru>
2025-03-06 17:32:06 +0300

    ADD private constructor

--- src/main/java/com/javarush/pukhov/view/console/constants/Messages.java
+++ src/main/java/com/javarush/pukhov/view/console/constants/Messages.java
@@ -1,6 +1,11 @@
 package com.javarush.pukhov.view.console.constants;
 
 public final class Messages {
+
+
+        private Messages() {
+        }
+
     public static final String REQUEST_SELECT_MODE = "Please select mode:";
     public static final String LINE = "-".repeat(20);
 

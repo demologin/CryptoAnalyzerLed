1e4823841b62b07cd7d7c930c4eb9dadaa642641
LyohaSmi <smileha@mail.ru>
2025-03-06 13:52:04 +0300

    ADD new Constants

--- src/main/java/com/javarush/smirnov/Constants.java
+++ src/main/java/com/javarush/smirnov/Constants.java
@@ -1,8 +1,13 @@
 package com.javarush.smirnov;
 
 public class Constants {
-    public static String[] start = {"program started", "choose an option:", "1. Encode", "2. Decode", "3. Exit"};
-    public static String incorInput = "incorrect input";
-    public static String end = "program ended";
-    public static String ok = "ok";
+    public static String[] start = {"Program started", "Choose option:", "1. Encode", "2. Decode", "3. Exit"};
+    public static String incorInput = "Incorrect input";
+    public static String corInput = "Correct input";
+    public static String end = "Program ended";
+    public static String ok = "Ok";
+    public static String key = "Enter key:";
+    public static String path = "Enter FULL path to file";
+    public static String pathSaveEncoding = "Enter FULL path to file for save encoding";
+    public static String pathSaveDecoding = "Enter FULL path to file for save decoding";
 }

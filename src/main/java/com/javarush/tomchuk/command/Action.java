ebf0c6bf9e752c4f517a25ef867330097bdd5a19
Nikita <tomchukn4@gmail.com>
2025-03-07 20:33:58 +0700

    ADD Action.java

--- src/main/java/com/javarush/tomchuk/command/Action.java
+++
@@ -1,8 +0,0 @@
-package com.javarush.tomchuk.command;
-
-import com.javarush.tomchuk.constant.ResultCode;
-
-public interface Action {
-
-    ResultCode execute(String sourceText, String destinationText);
-}
